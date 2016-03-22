0f2753e13285340f8c0688eddb48dcc4ba7d21a1
andreiamtf <andreiamtf@gmail.com>
2016-03-22 10:17:21 +0000

    ViewModel

---
+++ ViewModels/MusicCartViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MyMusic.Models;
+
+namespace MyMusic.ViewModels
+{
+
+    //  holds the contents of the user’s shopping cart, 
+    public class MusicCartViewModel
+    {
+        public List<Cart> CartAlbums { get; set; }
+        public decimal CartTotal { get; set; }
+
+
+
+
+    }
+}
